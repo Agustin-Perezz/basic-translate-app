5fadcb5d224d5acab755cb1bba3c6d0a7dca68a8
Agustin-Perezz <agustinnicolas806@gmail.com>
2025-07-02 11:29:18 -0300

    Add the logic to handle language change and reorder language component

---
+++ src/routes/LanguageSelector.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+  export let value: string;
+
+  let languages = [
+    { name: 'English' },
+    { name: 'Spanish' },
+    { name: 'French' },
+    { name: 'German' },
+    { name: 'Italian' },
+    { name: 'Portuguese' },
+    { name: 'Russian' },
+    { name: 'Chinese' },
+    { name: 'Japanese' },
+    { name: 'Arabic' }
+  ];
+</script>
+
+<select
+  bind:value
+  class="mb-2 w-28 rounded border border-gray-600 bg-gray-800 px-2 py-1 text-white"
+>
+  {#each languages as { name } (name)}
+    <option value={name}>{name}</option>
+  {/each}
+</select>
